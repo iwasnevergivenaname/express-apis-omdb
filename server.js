cf2b57e67a6f3fb4c5f8344ff78a7e8f8064c7a7
iwasnevergivenaname <hannah.mace93@gmail.com>
2020-08-07 21:08:32 -0700

    finished

--- server.js
+++ server.js
@@ -1,46 +1,65 @@
 require('dotenv').config();
-const  axios = require('axios').default;
-const express = require('express');
-const ejsLayouts = require('express-ejs-layouts');
+console.log(process.env.API_KEY)
+const express = require("express");
+const ejsLayouts = require("express-ejs-layouts");
+const axios = require("axios");
 const app = express();
 
-const omdbApiKey = '22715dd0';
+const PORT = 4200;
 
-// Sets EJS as the view engine
-app.set('view engine', 'ejs');
-// Specifies the location of the static assets folder
-app.use(express.static('static'));
-// Sets up body-parser for parsing form data
-app.use(express.urlencoded({extended: false}));
-// Enables EJS Layouts middleware
+const API_KEY = process.env.API_KEY;
+
+app.set("view engine", "ejs");
+app.use(express.static("static"));
+app.use(express.urlencoded({ extended: false }));
 app.use(ejsLayouts);
 
-// Adds some logging to each request
-app.use(require('morgan')('dev'));
+app.use(require("morgan")("dev"));
 
-// Routes
-app.get('/', async function (req, res) {
+app.get("/", (req, res) => {
   res.render("index");
-  // try {
-  //   const {data} = await axios.get('http://www.omdbapi.com/?i=tt3896198&apikey=22715dd0');
-  //   res.send(data);
-  // } catch (error) {
-  //   res.send(error);
-  // }
 });
 
 app.get("/results", (req, res) => {
-  console.log("this is the results")
-  res.render("results");
+  let qs = {
+    params: {
+      s: req.query.titleSearch,
+      apikey: API_KEY,
+    },
+  };
+  axios
+    .get("http://www.omdbapi.com", qs)
+    .then((response) => {
+      console.log(response.data);
+      let searchResults = response.data.Search;
+      res.render("results", { searchResults });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/movies/:id", (req, res) => {
+  let qs = {
+    params: {
+      i: req.params.id,
+      apikey: API_KEY,
+    },
+  };
+  axios
+    .get("http://www.omdbapi.com", qs)
+    .then((response) => {
+      let movieDetails = response.data;
+      res.render("detail", { movieDetails });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`lisenting to port #${PORT}`);
 })
 
-app.get("/detail", (req, res) => {
-  console.log("this is details");
-  res.render("detail");
-})
-
-// The app.listen function returns a server handle
-let server = app.listen(process.env.PORT || 3000);
-
-// We can export this server to other servers like this
-module.exports = server;
+module.exports = app;
